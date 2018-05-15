2d1369492070a71474faf70f70d247d0755d615f
ftudone <fabio_tudone@bmc.com>
2018-05-15 09:36:30 +0300

    Convert to Kotlin and Kotlin Gradle DSL

---
+++ src/main/kotlin/guru/springframework/DiDemoApplication.kt
@@ -0,0 +1,11 @@
+package guru.springframework
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DiDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DiDemoApplication>(*args)
+}
